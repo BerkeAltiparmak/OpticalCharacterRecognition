1a8e022deffd66b5c274ec967376d2afb0f801f9
Berke Altiparmak <altiparmak.berke@gmail.com>
2022-07-21 10:01:21 +0300

    Initial GoogleVision OCR set-up
    @TODO: make Google read data properly, as although it reads the characters almost perfectly, the order that it presents the text result is off due to the image having multiple columns.

---
+++ GoogleVision/src/main/java/org/example/Main.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import com.google.cloud.vision.v1.*;
+import com.google.protobuf.ByteString;
+
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        String filePath = "src/images/example4.png";
+
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+
+        ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
+
+        Image img = Image.newBuilder().setContent(imgBytes).build();
+        Feature feat = Feature.newBuilder().setType(Feature.Type.TEXT_DETECTION).build();
+        AnnotateImageRequest request =
+                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+        requests.add(request);
+
+        // Initialize client that will be used to send requests. This client only needs to be created
+        // once, and can be reused for multiple requests. After completing all of your requests, call
+        // the "close" method on the client to safely clean up any remaining background resources.
+        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+            client.close();
+
+            for (AnnotateImageResponse res : responses) {
+                if (res.hasError()) {
+                    System.out.format("Error: %s%n", res.getError().getMessage());
+                    return;
+                }
+
+                // For full list of available annotations, see http://g.co/cloud/vision/docs
+                for (EntityAnnotation annotation : res.getTextAnnotationsList()) {
+                    String text = annotation.getDescription();
+                    BoundingPoly boundingPoly = annotation.getBoundingPoly();
+
+                    System.out.format("Text: %s%n", text);
+                    System.out.format("Position : %s%n", boundingPoly);
+                }
+            }
+        }
+
+    }
+}
